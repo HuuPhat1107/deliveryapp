3b59c38618875c7d89409696850c46bc297c00b8
PhatDinh1101 <bosssy22@gmail.com>
2022-05-10 18:31:58 +0700

    up models

--- delivery/deliveryapp/models.py
+++ delivery/deliveryapp/models.py
@@ -60,7 +60,6 @@ class Status(models.Model):
 class Order(ModelBase):
     order_name = models.CharField(max_length=100, null=False)
     note = models.CharField(max_length=255, blank=True)
-
     image = models.ImageField(null=True, blank=True, upload_to='orders/%Y/%m')
     shipping_method = models.ForeignKey(ShippingMethod,
                                         null=True,
@@ -68,12 +67,32 @@ class Order(ModelBase):
                                         on_delete=models.CASCADE,
                                         )
     customer = models.ForeignKey(Customer, null=True, on_delete=models.CASCADE, related_name="orders")
-    status = models.ForeignKey(Status,null=True, default=1, on_delete=models.SET_NULL)
+    status = models.ForeignKey(Status, null=True, default=1, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.order_name
 
-# class OrderDetail(ModelBase):
+
+class Address(models.Model):
+    address = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.address
+
+
+class OrderDetail(ModelBase):
+    quality = models.CharField(max_length=2)
+    description = models.TextField(null=True, blank=True)
+    name = models.CharField(max_length=200, null=True)
+    image = models.ImageField(null=True, upload_to='products/%Y/%m')
+    phone_cus = models.ForeignKey(Customer, null=True, on_delete=models.CASCADE())
+    phone_shipper = models.ForeignKey(Shipper, null=True, on_delete=models.CASCADE())
+    note = models.TextField()
+    total = models.CharField(max_length=200, blank=True)
+    area = models.ForeignKey(Address, null=True, on_delete=models.CASCADE())
+
+    def __str__(self):
+        return self.name
 
 
 class ActionBase(models.Model):
@@ -89,13 +108,16 @@ class ActionBase(models.Model):
 
 class Rating(ActionBase):
     rate = models.SmallIntegerField(default=0)
+    shipper = models.ForeignKey(Shipper, null=False, on_delete=models.SET_NULL())
+    star = models.CharField(max_length=5, null=False)
+    comment = models.TextField()
 
 
-class Address(models.Model):
-    address = models.CharField(max_length=30)
+class AuctionHistory(models.Model):
+    shipper = models.ForeignKey(User, null=True, on_delete=models.SET_NULL())
+    order = models.ForeignKey(Order, null=True, on_delete=models.SET_NULL())
+    price = models.TextField()
 
-    def __str__(self):
-        return self.address
 
 
 
